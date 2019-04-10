a86e918ade40e8a3f45779917a89e2d51b499a70
xub1997 <42289281+xub1997@users.noreply.github.com>
2019-04-10 20:05:01 +0800

    2019-04-10 20:04
    编码、解码器

--- common/src/main/java/com/beidou/common/netty/codc/ClientDecoder.java
+++ common/src/main/java/com/beidou/common/netty/codc/ClientDecoder.java
@@ -10,16 +10,16 @@ import java.util.List;
 
 public class ClientDecoder extends ByteToMessageDecoder {
     /**
-     * ?????????????
+     * 数据包基本长度
      */
     public static int BASE_LENTH = 4 + 2 + 2 + 4;
 
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf in, List<Object> out) throws Exception {
-        //????????????????????
+        //可读长度必须大于基本长度
         if(in.readableBytes() >= BASE_LENTH){
 
-            //???????????index
+            //记录包头开始的index
             int beginReader = in.readerIndex();
 
             while(true){
@@ -28,17 +28,20 @@ public class ClientDecoder extends ByteToMessageDecoder {
                 }
             }
 
-            //????
+            //模块号
             short module = in.readShort();
-            //?????
+            //命令号
             short cmd = in.readShort();
-            //????
+            /**
+             * 状态码
+             */
             int stateCode = in.readInt();
-            //????
+            //长度
             int length = in.readInt();
 
+            //长度又变得不满足
             if(in.readableBytes() < length){
-                //????????
+                //还原读指针
                 in.readerIndex(beginReader);
                 //out.add(null);
                 return;
@@ -53,7 +56,7 @@ public class ClientDecoder extends ByteToMessageDecoder {
             response.setStateCode(stateCode);
             response.setData(data);
 
-            //???????????
+            //继续往下传递
             out.add(response);
 
         }
